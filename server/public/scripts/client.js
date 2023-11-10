0b0cec51f38deca291b8d4655a2ce26e143c276d
James Woods <james.d.t.woods@gmail.com>
2023-11-10 10:00:13 -0600

    finished GET route and render function, now setting up POST route

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,44 @@
 console.log('JS is sourced!');
+
+// get items on page load
+getItems();
+
+// get route
+function getItems() {
+    axios({
+        method: 'GET',
+        url: '/todos'
+      }).then(function(response) {
+        console.log('Got todos', response.data);
+        renderItems(response.data);
+      }).catch(function (error) {
+        console.log('error in todos get', error);
+      });
+};
+
+// render items on DOM
+function renderItems(itemLlist) {
+    // Store selector in variable
+    const itemTableBody = document.getElementById('to-do-table-body');
+    // Empty previous data
+    itemTableBody.innerHTML = '';
+    // Add all songs to table
+    for (let item of itemLlist) {
+        itemTableBody.innerHTML += (`
+        <tr data-itemId="${item.id}">
+            <td>${item.text}</td>
+            <td>${item.isComplete}</td>
+            <td><button id="mark-complete" onclick="markComplete(event)">Mark Complete</button></td>
+            <td><button id="delete-item" onclick="deleteItem(event)">Delete Item</button></td>
+        </tr>`
+        );
+    }
+}
+
+function markComplete(event) {
+
+};
+
+function deleteItem(event) {
+
+};
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
-
+// get route
+router.get('/', (req, res) => {
+const sqlQueryText = `
+    SELECT * FROM "todos"
+    ORDER BY "id";
+`;
+pool.query(sqlQueryText)
+    .then((dbResult) => {
+    console.log('dbResult.rows:', dbResult.rows)
+    res.send(dbResult.rows)
+    }).catch((dbError) => {
+    res.sendStatus(500)
+    })
+});
 
 
 module.exports = router;
